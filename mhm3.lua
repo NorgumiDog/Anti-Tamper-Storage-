e817adc33bd4b6f15bfef135a28e346c7ef76de0
NorgumiDog <ericmauricegreen2006@gmail.com>
2025-03-19 06:22:41 -0500

    Update mhm3.lua

--- mhm3.lua
+++ mhm3.lua
@@ -1,48 +1,10 @@
 local mt = getrawmetatable(game)
-local old = mt.__namecall
-setreadonly(mt, false)
-
-local style = ""
-
-local function update()
-    local inv = game:GetService("Players").LocalPlayer.PlayerGui.Main.Menus.Inventory.Menus.Styles.List
-    for _, v in pairs(inv:GetDescendants()) do
-        if v:IsA("ImageLabel") and v.Visible and v.Parent and v.Parent.Parent then
-            style = v.Parent.Parent.Name
-            return
-        end
-    end
-end
-
-pcall(update)
-
-mt.__namecall = newcclosure(function(self, ...)
-    local args = {...}
-    local method = getnamecallmethod()
-    
-    if method == "FireServer" and self.Name == "" and
-       (args[1] == "Attack" or args[1] == "Heavy" or args[1] == "Stomp") and
-       typeof(args[2]) == "table" then
-        if style ~= "" then
-            args[2]["Class"] = style
-        end
-        return old(self, unpack(args))
-    end
-    
-    return old(self, ...)
-end)
-
-setreadonly(mt, true)
-
-
-local mt = getrawmetatable(game)
 setreadonly(mt, false)
 local old_index = mt.__index
 
-local currentStyle = ""
-
-local function setStyle(styleName)
-    currentStyle = styleName
+getgenv().currentStyle = ""
+getgenv().setStyle = function(styleName)
+    getgenv().currentStyle = styleName
 
     mt.__index =
         newcclosure(
@@ -57,4 +19,30 @@ local function setStyle(styleName)
     )
 end
 
-getgenv().setStyle = setStyle
+getgenv().styles = {
+    "Jeet Kune Do",
+    "SAVAGE",
+    "Karate",
+    "Ninja",
+    "Kicker",
+    "Heavy Hitter",
+    "Aggressive",
+    "Hitman",
+    "Hawk",
+    "Muay Thai",
+    "CRASH OUT",
+    "Jaw Breaker",
+    "Philly",
+    "Stud",
+    "Squabble",
+    "Bones",
+    "Peak A Boo",
+    "Striker",
+    "Boxer",
+    "Slap Boxer",
+    "Amateur",
+    "Baddie",
+    "Compound V"
+}
+
+getgenv().get_styles = function() return getgenv().styles end
