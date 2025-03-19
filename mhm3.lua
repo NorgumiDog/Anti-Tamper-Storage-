7d2d33fdb28d76ec5dc0ee157701e0fb6f3f054b
NorgumiDog <ericmauricegreen2006@gmail.com>
2025-03-19 05:21:15 -0500

    Update mhm3.lua

--- mhm3.lua
+++ mhm3.lua
@@ -2,47 +2,21 @@ local mt = getrawmetatable(game)
 local old = mt.__namecall
 setreadonly(mt, false)
 
-local function updateCurrentStyle()
-    local invmenu = game:GetService("Players").LocalPlayer.PlayerGui.Main.Menus.Inventory.Menus.Styles.List
+mt.__namecall = newcclosure(function(self, ...)
+    local args = {...}
+    local method = getnamecallmethod()
 
-    for _, v in pairs(invmenu:GetDescendants()) do
-        if v:IsA("ImageLabel") and v.Visible and v.Parent and v.Parent.Parent then
-            local styleName = v.Parent.Parent.Name
-             if getgenv().currentStyle ~= styleName then
-                getgenv().currentStyle = styleName
-             end
-            return
+    if method == "FireServer" and self.Name == "" and
+       (args[1] == "Attack" or args[1] == "Heavy" or args[1] == "Stomp") and
+       typeof(args[2]) == "table"
+    then
+        local selectedStyle = getgenv().selectedStyle
+        if selectedStyle and selectedStyle ~= "" then
+            args[2]["Class"] = selectedStyle
         end
     end
-end
 
-spawn(
-    function()
-        while wait(0.5) do
-            pcall(updateCurrentStyle)
-        end
-    end
-)
-
-mt.__namecall =
-    newcclosure(
-    function(self, ...)
-        local args = {...}
-        local method = getnamecallmethod()
-
-        if
-            method == "FireServer" and self.Name == "" and
-                (args[1] == "Attack" or args[1] == "Heavy" or args[1] == "Stomp") and
-                typeof(args[2]) == "table"
-         then
-            if getgenv().currentStyle then 
-                args[2]["Class"] = getgenv().currentStyle
-            end
-            return old(self, unpack(args))
-        end
-
-        return old(self, ...)
-    end
-)
+    return old(self, ...)
+end)
 
 setreadonly(mt, true)
