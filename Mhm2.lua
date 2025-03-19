2f0eb0a81de45da5ace9e10867e2e987062bc55b
NorgumiDog <ericmauricegreen2006@gmail.com>
2025-03-19 04:26:33 -0500

    Create Mhm2.lua

---
+++ Mhm2.lua
@@ -0,0 +1,26 @@
+getgenv().selectedLimb = "Head"
+
+local limbs = {"Head", "LeftFoot", "LeftHand", "LeftLowerArm", "LeftLowerLeg", "LeftUpperArm", "LeftUpperLeg", "LowerTorso", "RightFoot", "RightHand", "RightLowerArm", "RightLowerLeg", "RightUpperArm", "RightUpperLeg", "UpperTorso"}
+
+local mt = getrawmetatable(game)
+local old = mt.__namecall
+setreadonly(mt, false)
+
+mt.__namecall = newcclosure(function(self, ...)
+    local args = {...}
+    local method = getnamecallmethod()
+
+    if method == "FireServer" and self.Name == "" then
+        local attackType = args[1]
+        if attackType == "Attack" or attackType == "Stomp" or attackType == "Heavy" then
+            local character = args[2]["Character"]
+            args[2]["Limb"] = getgenv().selectedLimb
+            args[2]["Hit"] = character[getgenv().selectedLimb]
+            return old(self, unpack(args))
+        end
+    end
+    
+    return old(self, ...)
+end)
+
+setreadonly(mt, true)
