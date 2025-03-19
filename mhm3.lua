398954d9e505c591aadb82b34395b8e9d8c593e3
NorgumiDog <ericmauricegreen2006@gmail.com>
2025-03-19 05:48:38 -0500

    Update mhm3.lua

--- mhm3.lua
+++ mhm3.lua
@@ -1,29 +1,12 @@
+local mt = getrawmetatable(game)
 local old = mt.__namecall
 setreadonly(mt, false)
 
-local currentStyle = ""
-
-local function updateCurrentStyle()
-    local invmenu = game:GetService("Players").LocalPlayer.PlayerGui.Main.Menus.Inventory.Menus.Styles.List
-
-    for _, v in pairs(invmenu:GetDescendants()) do
-        if v:IsA("ImageLabel") and v.Visible and v.Parent and v.Parent.Parent then
-            local styleName = v.Parent.Parent.Name
-            if currentStyle ~= styleName then
-                currentStyle = styleName
-            end
-            return
-        end
+local function updateSelectedStyle(args)
+    if getgenv().SelectedStyle ~= "" then
+        args[2]["Class"] = getgenv().SelectedStyle
     end
 end
-
-spawn(
-    function()
-        while wait(0.5) do
-            pcall(updateCurrentStyle)
-        end
-    end
-)
 
 mt.__namecall =
     newcclosure(
@@ -36,12 +19,12 @@ mt.__namecall =
                 (args[1] == "Attack" or args[1] == "Heavy" or args[1] == "Stomp") and
                 typeof(args[2]) == "table"
          then
-            if currentStyle ~= "" then
-                args[2]["Class"] = currentStyle
-            end
+            updateSelectedStyle(args) -- Call the function here
             return old(self, unpack(args))
         end
 
         return old(self, ...)
     end
 )
+
+setreadonly(mt, true)
